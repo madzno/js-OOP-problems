62d4312765ec3837b177882923d361fb8e5e3903
Madz No <madzno12@gmail.com>
2023-04-20 15:49:03 -0400

    adding object creation exercises

---
+++ object_creation/ancestors.js
@@ -0,0 +1,30 @@
+Object.prototype.ancestors = function () {
+  let currentPrototype = this;
+  let prototypeChain = [];
+
+  while (currentPrototype) {
+    currentPrototype = Object.getPrototypeOf(currentPrototype);
+
+    if (currentPrototype === Object.prototype) {
+      prototypeChain.push('Object.prototype');
+    } else if (currentPrototype !== null) {
+      prototypeChain.push(currentPrototype.name);
+    }
+  }
+
+  return prototypeChain;
+}
+
+// name property added to make objects easier to identify
+const foo = { name: 'foo' };
+const bar = Object.create(foo);
+bar.name = 'bar';
+const baz = Object.create(bar);
+baz.name = 'baz';
+const qux = Object.create(baz);
+qux.name = 'qux';
+
+console.log(qux.ancestors());  // returns ['baz', 'bar', 'foo', 'Object.prototype']
+console.log(baz.ancestors());  // returns ['bar', 'foo', 'Object.prototype']
+console.log(bar.ancestors());  // returns ['foo', 'Object.prototype']
+console.log(foo.ancestors());  // returns ['Object.prototype']
